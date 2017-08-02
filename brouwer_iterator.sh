fe386e69eec6cb0b55aa2f1e681f4ed5ab54a431
Alex Kenich <alex.kenich9@gmail.com>
2017-08-02 15:40:53 +0100

    Iterator now works with floating point numbers!
    Because Bash doesn't do floating point arithmetic, had to change the while loop comparator and other calc stuff. Use bc for all arithmetic from now on.

--- brouwer_iterator.sh
+++ brouwer_iterator.sh
@@ -3,23 +3,23 @@
 # This script runs the Brouwer diagram script at temperature $temp and the smallest possible $Convergence.
 # It will append the Brouwer diagram data to datadump.txt, and then increment $temp by $temp_increment
 
-temp=300
-temp_maximum=2500
+temp="300"
+temp_maximum="2500"
 Convergence=1e-40
-temp_increment=5
+temp_increment="5"
 
-while [ $temp -le $temp_maximum ]
+while [ "$(bc <<< "$temp < $temp_maximum") == "1" ]
 do
   perl defect_analysis3.pl intrinsic > failure.test
   echo Temperature = $temp
   echo Convergence = $Convergence
   if grep -Fxq "Could not deteremine the Fermi level that gives charge neutrality, recommned you examine your DFT energies" ./failure.test
   then
-  sed -i "s/Convergence : $Convergence/Convergence : $(python<<<"print($Convergence*10)")/g" intrinsic.input
+  sed -i -e "s/Convergence : $Convergence/Convergence : $(python<<<"print($Convergence*10)")/g" intrinsic.input
   Convergence=$(python<<<"print($Convergence*10)")
   else
   cat intrinsic.res >> datadump.txt
-  sed -i "s/Temperature : $temp/Temperature : $((temp+temp_increment))/g" intrinsic.input
-  temp=$((temp+temp_increment))
+  sed -i -e "s/Temperature : $temp/Temperature : $( bc <<< "$temp + $temp_increment" )/g" intrinsic.input
+  temp=$( bc <<< "$temp + $temp_increment" )
   fi
 done
